87522f343d390e9940251b5ff5528a46dcb2a366
mw-171 <meganbxwu@gmail.com>
2024-02-18 20:16:18 -0500

    clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <div className="font-bold text-2xl">hi</div>
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <AuthProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/hackerevents" element={<HackerEvents />} />
-          <Route path="/hackerevents/:id" element={<ViewEvent />} />
           <Route path="/:id" element={<ViewEvent />} />
-          {/* <Route 
-            path="/events" 
-            element={
-              ({ isAuthenticated }: { isAuthenticated: boolean }) => {
-                return isAuthenticated ? <ListEvents /> : <Login />;
-              }} 
-              }
-          /> */}
         </Routes>
       </Router>
     </AuthProvider>
