1007295aa85dd6183b605b0f55fe63611df50326
mw-171 <meganbxwu@gmail.com>
2024-04-13 23:52:55 -0400

    add selector

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -32,26 +32,29 @@ const SearchBar = ({
     }
   }, [input, allEvents, setFilteredEvents, selectedEventType]);
   return (
-    <input
-      type="text"
-      placeholder="Search events..."
-      value={input}
-      onChange={(e): void => {
-        setInput(e.target.value);
-      }}
-      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-    />
+    <>
+      <input
+        type="text"
+        placeholder="Search events..."
+        value={input}
+        onChange={(e): void => {
+          setInput(e.target.value);
+        }}
+        className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+      />
+      <select
+        value={selectedEventType}
+        onChange={(e) => setSelectedEventType(e.target.value)}
+        className="block rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+      >
+        {eventTypes.map((type) => (
+          <option key={type} value={type} className="bg-gray-800 rounded-md">
+            {type}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
 export default SearchBar;
-
-// setSearchTerm(event.target.value);
-// if (event.target.value !== "") {
-//   const filteredEvents = allEvents.filter((event) =>
-//     event.name.toLowerCase().includes(searchTerm.toLowerCase())
-//   );
-//   setFilteredEvents(filteredEvents);
-// } else {
-//   setFilteredEvents(allEvents);
-// }
